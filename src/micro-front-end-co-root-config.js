b57e84c19f255c805ab2b6ebeb55f1512993e4a4
CliffCrerar <cliff.crerar@gmail.com>
2020-08-09 05:05:54 +0200

    minor

--- src/micro-front-end-co-root-config.js
+++ src/micro-front-end-co-root-config.js
@@ -1,20 +1,42 @@
 import { registerApplication, start } from "single-spa";
+// import { registerApplication, start } from "single-spa";
+import * as isActive from "./activity-functions";
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
-});
 
 // registerApplication({
-//   name: "@micro-front-end-co/navbar",
-//   app: () => System.import("@micro-front-end-co/navbar"),
-//   activeWhen: ["/"]
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
 // });
 
-start({
-  urlRerouteOnly: true,
+const navBarApp = '@micro-front-end-co/navbar'
+registerApplication({
+  name: navBarApp,
+  app: () => System.import(navBarApp),
+  activeWhen: isActive.nav,
+  customProps: { domElement: document.getElementById('nav-container') }
 });
+
+
+registerApplication({
+  name: "@micro-front-end-co/page-one",
+  app: () => System.import("@micro-front-end-co/page-one"),
+  activeWhen: isActive.pageOne,
+  customProps: { domElement: document.getElementById('page-onne-container') }
+});
+
+registerApplication({
+  name: "@micro-front-end-co/page-two",
+  app: () => System.import("@micro-front-end-co/page-two"),
+  activeWhen: isActive.pageTwo,
+  customProps: { domElement: document.getElementById('page-two-container') }
+});
+
+// start({
+//   urlRerouteOnly: true,
+// });
+
+start();
